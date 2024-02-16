452155631fe3afb21bd0e6493a7d6e9651308b29
L1nar <122251664+L1nar@users.noreply.github.com>
2024-02-16 16:39:03 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -15,7 +15,6 @@ if (time === null) {
 
 console.log(time);
 
-
 localStorage.setItem('key', '1');
 localStorage.setItem('key', '2');
 let value1 = localStorage.getItem('key');
@@ -27,8 +26,41 @@ localStorage.removeItem('key');
 
 localStorage.setItem('counter', 0);
 
-let add = 0;
-
-while (add != 10) {
-    localStorage.getItem('counter', counter++);
+// localStorage.setItem('count', +localStorage.getItem('count') + 1);
+const count = +localStorage.getItem('count') + 1;
+if (count > 10) {
+    count = 0;
 }
+localStorage.setItem('count', count);
+console.log(`Вы ${count} раз обновили страницу`);
+
+console.log(`Количество записей в localStorage ${localStorage.length}`); // Количество записей в localStorage();
+
+// Получение ключа по номеру
+let key = localStorage.key(0);
+console.log(key);
+let value = localStorage.getItem(key);
+console.log(value);
+
+// Перебор хранилища по индексам
+for (let i = 0; i < localStorage.length; i++) {
+    let key = localStorage.key(i);
+    let value = localStorage.getItem(key);
+    console.log(value);
+}
+
+// Получить массив ключей и значений хранилища
+let keys = Object.keys(localStorage);
+let values = Object.values(localStorage);
+console.log(`Массив ключей: `, keys);
+console.log(`Массив значений: `, values);
+
+// Хранение структур в localStorage();
+let arr = [1, 2, 3, 4, 5];
+
+// JSON.stringify для преобразования объектов в JSON
+// метод JSON.stringify берёт объект и преобразует его в строку
+// Полученна строка json называется JSON - форматированным или сериализованным объектом
+// Мы можем отправить его по сети или поместить в обычное хранилище данных
+
+localStorage.setItem('arr', JSON.stringify(arr));
